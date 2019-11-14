925d02ca2e1dc922ee088ae16f9d16f0ac8b1d0d
Madeline OLeary <madeline.w.oleary@gmail.com>
2019-11-14 17:32:56 -0600

    moving files to proper path, started acceptance tests for site model (not finished)

---
+++ spec/models/site_spec.rb
@@ -0,0 +1,127 @@
+require 'rails_helper'
+require_dependency 'site'
+
+describe Site do
+
+  context '#determine_user' do
+    it "returns current_user if it exists inside of guardian object" do
+      user = Fabricate(:user)
+      site = Site.new(Guardian.new(user))
+
+      expect(site.determine_user).to eq(user)
+    end
+
+    it "returns nil if no current user exists inside of guardian object" do
+      user = nil
+      site = Site.new(Guardian.new(user))
+
+      expect(site.determine_user).to eq(nil)
+    end
+
+    # there is also another edge case where guardian is nil. i guess this is before a user is signed in...but for the moment i'm not sure how to test this
+  end
+
+  describe '#icij_project_names' do
+    context "user belong to icij project groups" do
+      it "returns an array of the user's icij project group names, after determining the user" do
+        user = Fabricate(:user)
+        site = Site.new(Guardian.new(user))
+        icij_group = Fabricate(:icij_group)
+        icij_group.add(user)
+
+        expect(site.icij_project_names).to eq([icij_group.name])
+
+        user = nil
+        site = Site.new(Guardian.new(user))
+
+        expect(site.icij_project_names).to eq([])
+      end
+
+      it "does not return the names of icij project groups to which the user is not a member" do
+        user = Fabricate(:user)
+        site = Site.new(Guardian.new(user))
+        icij_group = Fabricate(:icij_group)
+        another_icij_group = Fabricate(:icij_group)
+        icij_group.add(user)
+
+        expect(site.icij_project_names).to eq([icij_group.name])
+      end
+    end
+  end
+
+  describe '#fellow_icij_project_members' do
+    context 'user is nil' do
+      it "returns an empty array" do
+        user = nil
+        site = Site.new(Guardian.new(user))
+
+        expect(site.fellow_icij_project_members).to eq([])
+      end
+    end
+
+    context 'user is not a member of any projects' do
+      it 'returns an empty array' do
+        user = Fabricate(:user)
+        site = Site.new(Guardian.new(user))
+
+        icij_group = Fabricate(:icij_group)
+
+        expect(site.fellow_icij_project_members).to eq([])
+      end
+    end
+
+    context 'user is the only project member' do
+      it "returns the user's id alone in an array" do
+        user = Fabricate(:user)
+        site = Site.new(Guardian.new(user))
+
+        icij_group = Fabricate(:icij_group)
+        icij_group.add(user)
+
+        expect(site.fellow_icij_project_members).to eq([user.id])
+      end
+
+      it "does not return users with negative ids (programmatic users)" do
+        user = Fabricate(:user)
+        site = Site.new(Guardian.new(user))
+
+        icij_group = Fabricate(:icij_group)
+        icij_group.add(user)
+
+        expect((site.fellow_icij_project_members).all?(&:positive?)).to eq(true)
+      end
+    end
+
+    context 'multiple project members' do
+      it "returns the ids in an array" do
+        user = Fabricate(:user)
+        another_user = Fabricate(:user)
+        site = Site.new(Guardian.new(user))
+
+        icij_group = Fabricate(:icij_group)
+        icij_group.add(user)
+        icij_group.add(another_user)
+
+        expect(site.fellow_icij_project_members).to eq([user.id, another_user.id])
+      end
+
+      it "does not return users with negative ids (programmatic users)" do
+        user = Fabricate(:user)
+        another_user = Fabricate(:user)
+        site = Site.new(Guardian.new(user))
+
+        icij_group = Fabricate(:icij_group)
+        icij_group.add(user)
+        icij_group.add(another_user)
+
+        expect((site.fellow_icij_project_members).all?(&:positive?)).to eq(true)
+      end
+    end
+  end
+
+  describe '#available_icij_projects' do
+
+  end
+
+  
+end
