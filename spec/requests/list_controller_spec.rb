99f00b5c0d7f87dc1f538f7ce44d0fe82a103fa3
Madeline OLeary <madeline.w.oleary@gmail.com>
2019-11-18 10:59:03 -0600

    added tests to make sure users of projects can't see the topics for groups they're not a member of

--- spec/requests/list_controller_spec.rb
+++ spec/requests/list_controller_spec.rb
@@ -1,30 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ListController do
-  let(:group) { Fabricate(:icij_group) }
   describe '#group_topics' do
-    %i{user user2}.each do |user|
-      let(user) do
-        user = Fabricate(:user)
-        group.add(user)
-        user
-      end
-    end
-
-
-    private_cat = Fabricate(:category)
-    private_cat.set_permissions(group.id => 1)
-    private_cat.save
-
-    group.update!(categories: [private_cat])
-
-
-    let!(:topic) { Fabricate(:topic, category: private_cat, user: user) }
-    let!(:topic2) { Fabricate(:topic, user: user2) }
-    let!(:another_topic) { Fabricate(:topic) }
-
     describe 'when an invalid group name is given' do
       it 'should return the right response' do
+        group = Fabricate(:icij_group)
+
+        user = Fabricate(:user)
+        group.add(user)
+
+        user2 = Fabricate(:user)
+        group.add(user2)
+
+        private_cat = Fabricate(:category)
+        private_cat.set_permissions(group.id => 1)
+        private_cat.save
+
+        group.update!(categories: [private_cat])
+
+        topic =  Fabricate(:topic, category: private_cat, user: user)
+        topic2 = Fabricate(:topic, user: user2)
+        another_topic = Fabricate(:topic)
+
         get "/topics/groups/something.json"
 
         expect(response.status).to eq(404)
@@ -34,18 +31,53 @@ RSpec.describe ListController do
     describe 'for an anon user' do
       describe 'public visible group' do
         it 'should return the right response' do
+          group = Fabricate(:icij_group)
+
+          user = Fabricate(:user)
+          group.add(user)
+
+          user2 = Fabricate(:user)
+          group.add(user2)
+
+          private_cat = Fabricate(:category)
+          private_cat.set_permissions(group.id => 1)
+          private_cat.save
+
+          group.update!(categories: [private_cat])
+
+          topic =  Fabricate(:topic, category: private_cat, user: user)
+          topic2 = Fabricate(:topic, user: user2)
+          another_topic = Fabricate(:topic)
+
           get "/topics/groups/#{group.name}.json"
 
-          byebug
           expect(response.status).to eq(200)
           expect(JSON.parse(response.body)["topic_list"]).to be_present
         end
       end
 
       describe 'restricted group' do
-        before { group.update!(visibility_level: Group.visibility_levels[:staff]) }
-
         it 'should return the right response' do
+          group = Fabricate(:icij_group)
+
+          user = Fabricate(:user)
+          group.add(user)
+
+          user2 = Fabricate(:user)
+          group.add(user2)
+
+          private_cat = Fabricate(:category)
+          private_cat.set_permissions(group.id => 1)
+          private_cat.save
+
+          group.update!(categories: [private_cat])
+
+          group.update!(visibility_level: Group.visibility_levels[:members])
+
+          topic =  Fabricate(:topic, category: private_cat, user: user)
+          topic2 = Fabricate(:topic, user: user2)
+          another_topic = Fabricate(:topic)
+
           get "/topics/groups/#{group.name}.json"
 
           expect(response.status).to eq(403)
@@ -57,9 +89,10 @@ RSpec.describe ListController do
       before { sign_in(Fabricate(:user)) }
 
       describe 'restricted group' do
-        before { group.update!(visibility_level: Group.visibility_levels[:staff]) }
-
         it 'should return the right response' do
+          group = Fabricate(:icij_group)
+          group.update!(visibility_level: Group.visibility_levels[:members])
+
           get "/topics/groups/#{group.name}.json"
 
           expect(response.status).to eq(403)
@@ -68,20 +101,81 @@ RSpec.describe ListController do
     end
 
     describe 'for a group user' do
-      before do
+      it 'should be able to view the topics started by icij project group users' do
+        user = Fabricate(:user)
         sign_in(user)
-      end
 
-      it 'should be able to view the topics started by group users' do
+        group = Fabricate(:icij_group)
+        group.add(user)
+        group.update!(visibility_level: Group.visibility_levels[:members])
+
+        user2 = Fabricate(:user)
+        group.add(user2)
+
+        private_cat = Fabricate(:category)
+        private_cat.set_permissions(group.id => 1)
+        private_cat.save
+
+        group.update!(categories: [private_cat])
+
+        topic =  Fabricate(:topic, category: private_cat, user: user2)
+        topic2 =  Fabricate(:topic, category: private_cat, user: user)
+
         get "/topics/groups/#{group.name}.json"
 
         expect(response.status).to eq(200)
 
         topics = JSON.parse(response.body)["topic_list"]["topics"]
 
-        expect(topics.map { |topic| topic["id"] }).to contain_exactly(
+        expect(topics.map { |topic| topic["id"] }).to include(
           topic.id, topic2.id
         )
+      end
+
+      it "should not return topics started by non-icij project group users" do
+        user = Fabricate(:user)
+        sign_in(user)
+
+        group = Fabricate(:icij_group)
+        group.add(user)
+        group.update!(visibility_level: Group.visibility_levels[:members])
+
+        user2 = Fabricate(:user)
+        another_group = Fabricate(:icij_group)
+        another_group.add(user2)
+        another_group.update!(visibility_level: Group.visibility_levels[:members])
+
+        private_cat = Fabricate(:category)
+        private_cat.set_permissions(group.id => 1)
+        private_cat.save
+
+        another_private_cat = Fabricate(:category)
+        another_private_cat.set_permissions(another_group.id => 1)
+        another_private_cat.save
+
+        group.update!(categories: [private_cat])
+        another_group.update!(categories: [another_private_cat])
+
+        topic =  Fabricate(:topic, category: another_private_cat, user: user2)
+        topic2 =  Fabricate(:topic, category: private_cat, user: user)
+
+        get "/topics/groups/#{group.name}.json"
+
+        expect(response.status).to eq(200)
+
+        topics = JSON.parse(response.body)["topic_list"]["topics"]
+
+        expect(topics.map { |topic| topic["id"] }).to include(
+          topic2.id
+        )
+
+        expect(topics.map { |topic| topic["id"] }).to_not include(
+          topic.id
+        )
+
+        get "/topics/groups/#{another_group.name}.json"
+
+        expect(response.status).to eq(403)
       end
     end
   end
