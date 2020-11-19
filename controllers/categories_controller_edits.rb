90ee6e4c6b437fa794cb0def22454f0209586c65
madeline oleary <madeline.w.oleary@gmail.com>
2020-11-19 16:49:23 +0000

    cleaner handling of permissions verification

--- controllers/categories_controller_edits.rb
+++ controllers/categories_controller_edits.rb
@@ -1,42 +1,22 @@
-module ExtendCategoriesController
-  def create
-    guardian.ensure_can_create!(Category)
-    position = category_params.delete(:position)
+module CategoriesControllerExtension
+  private
 
-    @category =
-      begin
-        Category.new(category_params.merge(user: current_user))
-      rescue ArgumentError => e
-        return render json: { errors: [e.message] }, status: 422
-      end
-
+  def verify_permissions
     if params[:permissions].nil?
-      @category.errors[:base] << "Please assign a project to this group."
-      return render_json_error(@category)
+      return render json: { errors: ['Please assign a project to this group.'] }, status: 400
     end
 
     icij_groups = Group.visible_icij_groups(current_user).pluck(:name)
     has_permission = icij_groups.any? { |group| (params[:permissions].keys).include? group }
 
     unless has_permission
-      @category.errors[:base] << "You are not a member of this project."
-      return render_json_error(@category)
-    end
-
-    if @category.save
-      @category.move_to(position.to_i) if position
-
-      Scheduler::Defer.later "Log staff action create category" do
-        @staff_action_logger.log_category_creation(@category)
-      end
-
-      render_serialized(@category, CategorySerializer)
-    else
-      return render_json_error(@category) unless @category.save
+      return render json: { errors: ['You are not a member of this project.'] }, status: 403
     end
   end
 end
 
 class ::CategoriesController
-  prepend ExtendCategoriesController
+  before_action :verify_permissions, only: [:create, :update]
+
+  prepend CategoriesControllerExtension
 end
